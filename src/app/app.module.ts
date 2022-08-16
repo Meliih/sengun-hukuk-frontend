b73c9b17952bf0f6adb61a2825fd587bb2180b86
Meliih <melihkahraman4@gmail.com>
2022-08-16 16:58:22 +0300

    side bar menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import { SideBarComponent } from './side-bar/side-bar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    SideBarComponent
+    
   ],
   imports: [
     BrowserModule,
